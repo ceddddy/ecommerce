cc3ff69ce34d1b566f82864883c68de0da6c2908
Ceddy <ceddddyadvance@gmail.com>
2022-09-11 00:47:23 +0200

    finished with schemas and backend

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -5,10 +5,8 @@ import { Product, FooterBanner, HeroBanner } from "../components";
 
 const Home = ({ products, bannerData }) => (
   <div>
-    <HeroBanner />
-    {console.log(bannerData)}
-    {console.log(products)}
-
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+    
     <div className="products-heading">
       <h2>Best Selling Products</h2>
       <p>Parts of many variations</p>
@@ -25,7 +23,7 @@ export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
   const products = await client.fetch(query);
 
-  const bannerQuery = '[_type == "banner"]';
+  const bannerQuery = '*[_type == "banner"]';
   const bannerData = await client.fetch(bannerQuery);
 
   return {
